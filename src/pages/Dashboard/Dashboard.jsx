9b8e4c6d5a13d2b39bb863097279464c537a05b9
Princewill Awili <princewill.awili@gmail.com>
2022-12-27 22:37:58 +0100

    Created Sidebar Layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,9 @@
 import './dashboard.css'
 import smallLogo from '../../assets/images/logo.svg'
+import searchIcon from '../../assets/icons/SearchIcon.svg'
+import {MdOutlineNotifications as NotificationIcon} from 'react-icons/md'
+import userPic from '../../assets/images/userPic.svg'
+import ArrowDown from '../../assets/icons/arrowDown.svg'
 
 const Dashboard = () => {
   return (
@@ -8,9 +12,24 @@ const Dashboard = () => {
         <img src={smallLogo} alt="lendsqr logo" className='smLogo' />
 
         <div className="searchBox">
-          <input type="text" className="searchBar" />
+          <input type="text" className="searchBar" placeholder='Search for anything' />
+          <div className="searchBtn">
+            <img src={searchIcon} alt='search icon' className='searchIcon'/>
+          </div>
         </div>
 
+        <div className="topbarRight">
+          <p className="docs">Docs</p>
+          <NotificationIcon className='notIcon'/>
+          <img src={userPic} alt="user pic" className='userPic'/>
+          <p className="userName">Adedeji</p>
+          <img src={ArrowDown} alt='arrowDown' className='arrowDown'/>
+        </div>
+
+      </div>
+
+      <div className="sidebar">
+        
       </div>
     </div>
   )
