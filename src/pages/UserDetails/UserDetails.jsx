93e0b97f0fc3c9df55664ee6786360f77b9b4c41
Princewill Awili <princewill.awili@gmail.com>
2022-12-30 19:59:24 +0100

    Started UserDetails

--- src/pages/UserDetails/UserDetails.jsx
+++ src/pages/UserDetails/UserDetails.jsx
@@ -2,6 +2,7 @@ import './userdetails.css'
 
 import Topbar from '../../components/Topbar/Topbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import backIcon from '../../assets/icons/backArrow.svg'
 
 
 const UserDetails = () => {
@@ -9,7 +10,23 @@ const UserDetails = () => {
     <div className='userDetails'>
       <Topbar/>
       <Sidebar/>
-      
+      <div className="userInfo">
+
+        <div className="back">
+          <img src={backIcon}/>
+          <span className="backTxt">Back to Users</span>
+        </div>
+
+        <div className="userActions">
+          <h2 className="userDetailsTitle">User Details</h2>
+          <div className="actionBtns">
+            <span className="blacklistBtn actionBtn">BLACKLIST USER</span>
+            <span className="activateBtn actionBtn">ACTIVATE USER</span>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
   )
 }
